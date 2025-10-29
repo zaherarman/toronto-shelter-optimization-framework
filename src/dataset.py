3205ab5d81d1e6677122f9bcb178971cf99fae58
Zaher Arman <a.zaher@mail.utoronto.ca>
2025-10-29 00:31:24 -0400

    Formatting

--- src/dataset.py
+++ src/dataset.py
@@ -16,8 +16,12 @@ for idx, resource in enumerate(package["result"]["resources"]):
         resource_metadata = requests.get(url).json()
 
         file_url = resource_metadata["result"]["url"]
-        file_name = resource_metadata["result"]["name"].replace("/", "_") + "." + resource_metadata["result"]["format"].lower()
-        
+        file_name = (
+            resource_metadata["result"]["name"].replace("/", "_")
+            + "."
+            + resource_metadata["result"]["format"].lower()
+        )
+
         print(f"Downloading {file_name} from {file_url}")
         response = requests.get(file_url)
 
@@ -33,7 +37,7 @@ bad = RAW_DATA_DIR / "shelter-locations-wgs84.shp"
 # What it should be
 good = RAW_DATA_DIR / "shelter-locations-wgs84.zip"
 
-# Rename it only if it’s really a .zip file 
+# Rename it only if it’s really a .zip file
 if bad.exists() and zipfile.is_zipfile(bad):
     bad.rename(good)
     print(f"Renamed to {good.name}")
@@ -47,22 +51,22 @@ with zipfile.ZipFile(good, "r") as z:
 good.unlink()
 
 # Daily Shelter & Overnight Service Occupancy & Capacity
-url = base_url + "/api/3/action/package_show" 
-params = { "id": "daily-shelter-overnight-service-occupancy-capacity"}
-package = requests.get(url, params = params).json()
-		
-for idx, resource in enumerate(package["result"]["resources"]):	
+url = base_url + "/api/3/action/package_show"
+params = {"id": "daily-shelter-overnight-service-occupancy-capacity"}
+package = requests.get(url, params=params).json()
+
+for idx, resource in enumerate(package["result"]["resources"]):
     name = resource["name"].lower()
-    
+
     if "2024" in name and resource["format"].lower() == "csv":
         if not resource["datastore_active"]:
             url = base_url + "/api/3/action/resource_show?id=" + resource["id"]
             resource_metadata = requests.get(url).json()
             print(resource_metadata)
-            
+
             file_url = resource_metadata["result"]["url"]
             file_name = resource_metadata["result"]["name"].replace("/", "_")
-            
+
             print(f"Downloading {file_name} from {file_url}")
             response = requests.get(file_url)
 
@@ -73,9 +77,9 @@ for idx, resource in enumerate(package["result"]["resources"]):
             print(f"Saved {file_path}")
 
 # Toronto Shelter System Flow
-url = base_url + "/api/3/action/package_show" 
-params = { "id": "toronto-shelter-system-flow"}
-package = requests.get(url, params = params).json()
+url = base_url + "/api/3/action/package_show"
+params = {"id": "toronto-shelter-system-flow"}
+package = requests.get(url, params=params).json()
 
 for idx, resource in enumerate(package["result"]["resources"]):
     if not resource["datastore_active"]:
@@ -84,7 +88,7 @@ for idx, resource in enumerate(package["result"]["resources"]):
 
         file_url = resource_metadata["result"]["url"]
         file_name = resource_metadata["result"]["name"].replace("/", "_")
-        
+
         print(f"Downloading {file_name} from {file_url}")
         response = requests.get(file_url)
 
@@ -95,7 +99,6 @@ for idx, resource in enumerate(package["result"]["resources"]):
         print(f"Saved {file_path}")
 
 # Neighbourhood Profiles
-url = base_url + "/api/3/action/package_show" 
-params = { "id": "neighbourhood-profiles"}
-package = requests.get(url, params = params).json()
-
+url = base_url + "/api/3/action/package_show"
+params = {"id": "neighbourhood-profiles"}
+package = requests.get(url, params=params).json()
